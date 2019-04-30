ce7f719cc77a01a3dbe5b2715980ca3066a77213
Ty Mazey <tymazey@gmail.com>
2019-04-30 17:08:04 -0600

    user creation is done?

--- models/user.js
+++ models/user.js
@@ -1,12 +1,37 @@
 'use strict';
+var bcrypt = require('bcrypt');
+var hat = require('hat');
+var saltRounds = 10;
+var pry = require('pryjs');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
     password: DataTypes.STRING,
     api_key: DataTypes.STRING
   }, {});
   User.associate = function(models) {
     // associations can be defined here
   };
+
+  // class methods
+  User.validateNew = function(body) {
+    if (body.password === body.password_confirmation) {
+      var user = User.create({
+        email: body.email,
+        password: bcrypt.hashSync(body.password, saltRounds),
+        api_key: hat()
+      })
+    }
+    return user
+  };
+
+  /* lat line */
   return User;
 };
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../../../models').User;
+var pry = require('pryjs');
+
+
+/* POST new user */
+router.post('/', function(req, res, next) {
+  User.validateNew(req.body)
+  .then(user => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(201).send(JSON.stringify({ap_ikey: user.api_key}));
+  })
+  .catch(error => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(500).send({error});
+  });
+});
 
 
 module.exports = router;
