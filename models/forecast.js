2112d3d2ac5892a14543e4d4d3229739914b84aa
Ty Mazey <tymazey@gmail.com>
2019-05-02 14:56:37 -0600

    adds functions to forecast object for making full forecast object

--- models/forecast.js
+++ models/forecast.js
@@ -1,9 +1,22 @@
 export default class Forecast {
   constructor(location, weather) {
     this.location = location
-    this.weather = weather
-    this.currently = {}
-    this.hourly = {}
-    this.daily = {}
+    this.currently = new CurrentForecast(weather)
+    this.hourly = setHourly(weather)
+    this.daily = setDaily(weather)
+  }
+
+  setHourly(weather) {
+    var hourlyArray = []
+    for (var i = 0; i < 8; i++) {
+      dailyArray.push(new HourlyWeather(weather.hourly.data[i]))
+    }
+  }
+
+  setDaily(weather) {
+    var dailyArray = []
+    for (var i = 0; i < 7; i++) {
+      dailyArray.push(new DailyWeather(weather.daily.data[i]))
+    }
   }
 }
